f9315c8e85c2bfb8452a08b6fb3d58e1d23bd774
E-cell0 <114798678+E-cell0@users.noreply.github.com>
2023-03-23 00:02:13 +0900

    20230323

---
+++ aaa/src/aaaa.java
@@ -0,0 +1,18 @@
+public class aaaa {
+	
+	
+	
+	 public static void main(String[] args) {
+	        try {
+	            String number = null;
+	            int length = number.length();
+	            System.out.println("hello");
+	            System.out.println(number.length());
+	        } catch (Exception e) {
+
+	        } finally {
+
+	        }
+	    }
+	
+}
